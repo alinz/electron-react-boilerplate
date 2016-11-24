dc71e85390780e8c7eb89029b889992419516048
Ali Najafizadeh <a.najafizadeh@gmail.com>
2016-11-24 16:36:50 -0500

    removed redux

---
+++ app/components/Home/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import styles from './styles.css'
+
+export class Home extends Component {
+  render() {
+    return (
+      <div>
+        This is Home Component
+      </div>
+    )
+  }
+}
